0ee8ee5b62186878fd43aebcc58d963834cbcfd9
ELcajucom <elionore.cajucom@gmail.com>
2021-12-26 01:33:01 +0800

    ds

---
+++ bcpcdashboard.php
@@ -0,0 +1,13 @@
+<?php
+$title = 'Documents';
+require_once 'department/form_bcpc.php'; 
+?>
+<?php 
+echo '1';
+?>
+  <br>
+    <br>
+    <br>
+    <br>
+    <br>
+<?php require_once 'resources/footer.php'; ?>
