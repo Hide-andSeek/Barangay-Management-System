270cd74e372b32f1d6ac744cef502dbdedfd986f
Ggieman <96552202+Ggieman@users.noreply.github.com>
2021-12-26 18:41:00 +0800

    try

--- department/form_lupon.php
+++ department/form_lupon.php
@@ -107,7 +107,7 @@ if(!isset($_SESSION["employee_no"])){
 			  <section class="top-section">
 				  <div class="top-content">
 					<div>
-						<h5>Lupon
+						<h5>Lupon Department
 						<a href="#" class="circle">
 							 <img src="img/dt.png" >
 					    </a>
